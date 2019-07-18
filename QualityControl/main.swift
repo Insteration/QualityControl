febef82258460fdc1d4cc116843acffc82f3dfa4
Artem <inste@icloud.com>
2019-07-18 20:42:21 +0300

    Initial Commit

---
+++ QualityControl/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  QualityControl
+//
+//  Created by Артём Кармазь on 7/18/19.
+//  Copyright © 2019 Артём Кармазь. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
